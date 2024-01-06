ceed15d2b3d608a1ee648d1e1ac0c5a93a8b4afc
servik-code <ervik.soren@gmail.com>
2024-01-06 13:06:49 -0600

    Increment the number

---
+++ PlusOne.js
@@ -0,0 +1,57 @@
+/*
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+Example 3:
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+ 
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+*/
+
+function plusOne(digits) {
+    let n = digits.length;
+  
+    // Start from the rightmost digit
+    for (let i = n - 1; i >= 0; i--) {
+      if (digits[i] !== 9) {
+        // If the digit is not 9, simply increment it and return the result
+        digits[i]++;
+        return digits;
+      } else {
+        // If the digit is 9, set it to 0 and continue to the next left digit
+        digits[i] = 0;
+      }
+    }
+  
+    // If we reach here, it means all digits were 9, and we need to add a new leading 1
+    digits.unshift(1);
+  
+    return digits;
+  }
